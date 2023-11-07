71eda98ecc7b1652d7002216b753ea9046268a9d
Shubhajit Paul <shubhajitpaul717@gmail.com>
2023-11-07 13:59:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        cards.forEach(card => card.classList.remove("active"));
+        card.classList.add("active");
+    });
+});
